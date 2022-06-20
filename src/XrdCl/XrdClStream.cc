210a35b69d95d96944e297b6cb7d6817e1b0ccc7
David Smith <dhsmith@pmpe01.cern.ch>
2022-06-20 10:17:42 +0200

    [XrdCl] Avoid race condition in AsyncSocketHander on use of reader/writer objects after link is re-enabled
    Conflicts:
    	src/XrdClHttp

--- src/XrdCl/XrdClStream.cc
+++ src/XrdCl/XrdClStream.cc
@@ -993,10 +993,8 @@ namespace XrdCl
   //----------------------------------------------------------------------------
   // Call back when a message has been reconstructed
   //----------------------------------------------------------------------------
-  void Stream::OnReadTimeout( uint16_t substream, bool &isBroken )
+  void Stream::OnReadTimeout( uint16_t substream )
   {
-    isBroken = false;
-
     //--------------------------------------------------------------------------
     // We only take the main stream into account
     //--------------------------------------------------------------------------
@@ -1052,7 +1050,6 @@ namespace XrdCl
                                             *pChannelData );
     if( !st.IsOK() )
     {
-      isBroken =  true;
       scopedLock.UnLock();
       OnError( substream, st );
     }
